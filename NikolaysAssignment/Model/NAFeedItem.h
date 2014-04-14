3b1fe3f23c91dcb8ec7765b5e29f0499679935b2
Nikolay Aleschenko <nick130586@gmail.com>
2014-04-14 20:42:02 +0300

    Add feed item data class

---
+++ NikolaysAssignment/Model/NAFeedItem.h
@@ -0,0 +1,16 @@
+//
+//  NAFeedItem.h
+//  NikolaysAssignment
+//
+//  Created by Nikolay Aleschenko on 4/14/14.
+//  Copyright (c) 2014 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NAFeedItem : NSObject
+
+@property (nonatomic, strong) NSString *bodyText;
+@property (nonatomic, strong) NSString *identifier;
+
+@end
---
+++ NikolaysAssignment/Model/NAFeedItem.m
@@ -0,0 +1,13 @@
+//
+//  NAFeedItem.m
+//  NikolaysAssignment
+//
+//  Created by Nikolay Aleschenko on 4/14/14.
+//  Copyright (c) 2014 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import "NAFeedItem.h"
+
+@implementation NAFeedItem
+
+@end
