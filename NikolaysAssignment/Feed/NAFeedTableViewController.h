3db82ca41d80494b883d0a1405d3d9d73d18c257
Nikolay Aleschenko <nick130586@gmail.com>
2014-04-15 13:08:39 +0300

    Add empty tableView controller

---
+++ NikolaysAssignment/Feed/NAFeedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NAFeedTableViewController.h
+//  NikolaysAssignment
+//
+//  Created by Nikolay Aleschenko on 4/14/14.
+//  Copyright (c) 2014 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NAFeedTableViewController : UITableViewController
+
+@end
---
+++ NikolaysAssignment/Feed/NAFeedTableViewController.m
@@ -0,0 +1,77 @@
+//
+//  NAFeedTableViewController.m
+//  NikolaysAssignment
+//
+//  Created by Nikolay Aleschenko on 4/14/14.
+//  Copyright (c) 2014 Nikolay Aleshchenko. All rights reserved.
+//
+
+#import "NAFeedTableViewController.h"
+
+@interface NAFeedTableViewController ()
+
+@end
+
+@implementation NAFeedTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    
+    return self;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationController.navigationBar.hidden = NO;
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    if ([self isMovingFromParentViewController]) {
+        self.navigationController.navigationBar.hidden = YES;
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
