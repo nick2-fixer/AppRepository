aa4a158a08f8eaf8dc0a3e4fcd8a005a2a643468
Nikolay Aleschenko <nick130586@gmail.com>
2014-04-15 16:34:51 +0300

    Implement simple fetching of Twitter feed

--- NikolaysAssignment/DataFetchers/NATwitterDataFetcher.m
+++ NikolaysAssignment/DataFetchers/NATwitterDataFetcher.m
@@ -7,6 +7,11 @@
 //
 
 #import "NATwitterDataFetcher.h"
+#import "SA_OAuthTwitterEngine.h"
+
+static NSString *userName = @"nick130586";
+static NSString *consumerKey = @"XzXRNM5ogp2kdFHQtpz8hlvTR";
+static NSString *consumerSecret = @"NyE32ws2i1dfZHFvr5J4daDlUSsjrd8Bxi5NxENTiiM7fXfF4Z";
 
 @implementation NATwitterDataFetcher
 
@@ -21,5 +26,24 @@
     return sharedFetcher;
 }
 
+- (void)fetchDataWithFinishBlock:(void (^)(NSData *, NSError *, BOOL))finishBlock {
+    SA_OAuthTwitterEngine *engine = [[SA_OAuthTwitterEngine alloc] initOAuthWithDelegate:self];
+    engine.consumerKey = consumerKey;
+    engine.consumerSecret = consumerSecret;
+    
+    [engine getUserTimelineFor:@"nick130586" sinceID:0 startingAtPage:0 count:20];
+}
+
+- (void)requestSucceeded:(NSString *)connectionIdentifier {
+    
+}
+
+- (void)requestFailed:(NSString *)connectionIdentifier withError:(NSError *)error {
+    
+}
+
+- (void)statusesReceived:(NSArray *)statuses forRequest:(NSString *)connectionIdentifier {
+    NSLog(@"%@", statuses);
+}
 
 @end
