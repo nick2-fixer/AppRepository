72626c75c763c45774e35e48d928ea91ef9155cb
Mykola-Aleshchenko-SoftServe <nales@macmini897.ddns.softservecom.com>
2016-04-13 19:17:19 +0300

    Remove unused code

--- NikolaysAssignment/NAAppDelegate.m
+++ NikolaysAssignment/NAAppDelegate.m
@@ -24,12 +24,6 @@
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
